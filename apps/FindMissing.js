adc6f83ca3ffc81ac9fe4c3a485d3a90c8729cb8
Neto Onyiaji <onyiajineto@gmail.com>
2017-03-23 06:28:52 +0100

    Update according to airbnb guidelines

--- apps/FindMissing.js
+++ apps/FindMissing.js
@@ -1,7 +1,7 @@
 module.exports = {
 findMissing :function (arr1,arr2) {
 	
-	if ((arr1.length === 0) && (arr2.length === 0))  {
+	if ((arr1.length === 0) && (arr2.length === 0))  {       //this line of code returns if the length of the  two arrays are zero
 		return 0;
 	}
 	
@@ -9,13 +9,13 @@ findMissing :function (arr1,arr2) {
 	var sumarr1 = 0;
 	var sumarr2 = 0;
 	for (var i in arr1){
-		sumarr1 += arr1[i];
+		sumarr1 += arr1[i];				//this finds the sum of the first array
 	}
 	
 	for(var j in arr2){
-		sumarr2 += arr2[j];
+		sumarr2 += arr2[j];				//this finds the sum of the second array
 	}	
-	return ((sumarr1 - sumarr2)*(arr1.length - arr2.length));
+	return ((sumarr1 - sumarr2)*(arr1.length - arr2.length));	//this line of code is a mathematical expression that returns the missing number
 	
   
   }
